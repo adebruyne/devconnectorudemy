120e542b998527d0c2db0300aaaf95d2192ae243
Aylin DeBruyne <adebruyne88@gmail.com>
2018-10-27 19:05:44 -0400

    create Edit-Profile component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import { clearCurrentProfile } from "./actions/profileAction";
+import EditProfile from "./components/edit-profile/Edit-profile";
 
 import "./App.css";
 //Check for token
@@ -58,6 +59,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
