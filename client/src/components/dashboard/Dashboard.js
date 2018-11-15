c234cb7a6b4ed97e8e10318a0bbe2f77926b9f9e
Aylin DeBruyne <adebruyne88@gmail.com>
2018-11-15 15:53:25 -0500

    brings in Education component to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -34,6 +34,7 @@ class Dashboard extends Component {
             </p>
             <ProfileActions />
             <Experience experience={profile.experience} />
+            <Education education={profile.education} />
             <div style={{ marginBottom: "60px" }} />
             <button
               onClick={this.onDeleteClick.bind(this)}
