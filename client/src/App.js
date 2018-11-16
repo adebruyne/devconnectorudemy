1a1e8bc2af5b6ea5787330ea2ba2a33ebf867a02
Aylin DeBruyne <adebruyne88@gmail.com>
2018-11-16 18:31:41 -0500

    added flow of profiles with redux

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import { clearCurrentProfile } from "./actions/profileAction";
 import EditProfile from "./components/edit-profile/Edit-profile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
-
+import Profiles from "./components/profiles/Profiles";
 
 import "./App.css";
 //Check for token
@@ -54,6 +54,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -12,17 +12,31 @@ class Profiles extends Component {
     const { profiles, loading } = this.props.profile;
     let profileItems;
 
-    if (profile === null || loading) {
+    if (profiles === null || loading) {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
-        <h1>PROFILES HERE</h1>;
+        profileItems = <h1>PROFILES HERE</h1>;
       } else {
         profileItems = <h4>No profiles found...</h4>;
       }
     }
 
-    return <div />;
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with developers
+              </p>
+              {profileItems}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
